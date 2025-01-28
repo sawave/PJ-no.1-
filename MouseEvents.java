5a8ccce6878b6fdf7567991537faf6dcdd5cd42a
Saw Averil <162364975+sawave@users.noreply.github.com>
2025-01-28 14:48:28 +0530

    Add files via upload

---
+++ MouseEvents.java
@@ -0,0 +1,35 @@
+//Program to implement mouse events
+
+java.io.
+java.utils.scanner
+java.awt
+
+{
+
+	public class static void main String[args]() Mouse Events for Mouse Listener
+
+	set Label L
+	set Layout()
+	set Boundaries()[100][140][180][120]
+	add background[BLUE]
+
+	{
+
+		setMouseEvent.mouseClicked;
+		System.out.println("Mouse Clicked");
+
+		setMouseEvent.mouseIn;
+		System.out.println("Mouse In");
+
+		setMouseEvent.mouseOut;
+		System.out.println("Mouse Out");
+
+		setMouseEvent.mouseHeld;
+		System.out.println("Mouse Held");
+
+		setMouseEvent.mouseReleased;
+		System.out.println("Mouse Released");
+
+	}
+
+}
